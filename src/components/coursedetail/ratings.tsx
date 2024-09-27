f70f2cc626091c6c974bb9c5271eaf9ab687e877
Bob Moriasi <107801004+bobbrysonn@users.noreply.github.com>
2024-09-27 11:57:26 -0400

    Feat: now using computed ratings

--- src/components/coursedetail/ratings.tsx
+++ src/components/coursedetail/ratings.tsx
@@ -1,20 +1,30 @@
-import {Card, CardContent} from "@/components/ui/card";
+import { Card, CardContent } from "@/components/ui/card";
 
-export default function Ratings() {
+export default function Ratings({
+  averageRating,
+  calledItLayup,
+}: {
+  averageRating: number;
+  calledItLayup: number;
+}) {
   return (
     <div className="flex justify-between mx-auto max-w-3xl mt-6 md:mt-10">
       <Card className="max-w-56 w-full">
         <CardContent className="flex flex-col gap-2 items-center justify-center p-4">
-          <p className="text-4xl font-bold">78</p>
-          <p className="text-sm mb-2 text-[#333333] dark:text-muted-foreground">Average Rating</p>
+          <p className="text-4xl font-bold">{averageRating}</p>
+          <p className="text-sm mb-2 text-[#333333] dark:text-muted-foreground">
+            Average Rating
+          </p>
         </CardContent>
       </Card>
       <Card className="max-w-56 w-full">
         <CardContent className="flex flex-col gap-2 items-center justify-center p-4">
-          <p className="text-4xl font-bold">11</p>
-          <p className="text-sm mb-2 text-[#333333] dark:text-muted-foreground">called it a layup</p>
+          <p className="text-4xl font-bold">{calledItLayup}</p>
+          <p className="text-sm mb-2 text-[#333333] dark:text-muted-foreground">
+            called it a layup
+          </p>
         </CardContent>
       </Card>
     </div>
-  )
+  );
 }
