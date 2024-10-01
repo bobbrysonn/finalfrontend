3fdd056c23d2412b0513543d6ff3d2a8d7f1913b
Bob Moriasi <107801004+bobbrysonn@users.noreply.github.com>
2024-10-01 00:22:36 -0400

    Revert "Added spotlight"
    This reverts commit fd95208eb927b79eb5d4d6fccbaaaf284d87992d.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,25 +9,6 @@ const config: Config = {
   ],
   theme: {
   	extend: {
-			screens: {
-				'3xl': '2048px',
-				'4xl': '2560px',
-			},
-			animation: {
-				spotlight: "spotlight 2s ease .75s 1 forwards",
-			},
-			keyframes: {
-				spotlight: {
-          "0%": {
-            opacity: 0,
-            transform: "translate(-72%, -62%) scale(0.5)",
-          },
-          "100%": {
-            opacity: 1,
-            transform: "translate(-50%,-40%) scale(1)",
-          },
-				},
-			},
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
