9f4c253dbbd3a0ab751725bcb4152986ee44105e
Bob Moriasi <107801004+bobbrysonn@users.noreply.github.com>
2024-09-30 21:31:30 -0400

    Added metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
-import type {Metadata} from "next";
-import {Inter} from "next/font/google";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-import {ThemeProvider} from "@/providers/theme-provider";
-import {Toaster} from "@/components/ui/toaster";
+import { ThemeProvider } from "@/providers/theme-provider";
+import { Toaster } from "@/components/ui/toaster";
 import Header from "@/components/header";
 
 const inter = Inter({
@@ -12,26 +12,76 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "Based Reviews",
+  applicationName: "Based Reviews",
+  authors: { name: "Bob Moriasi", url: "https://bobbrysonn.dev" },
+  creator: "Bob Moriasi",
   description: "Based course reviews for dartmouth students",
+  keywords: [
+    "Based",
+    "Based Reviews",
+    "Course",
+    "Course Reviews",
+    "Dartmouth",
+    "Dartmouth Based Reviews",
+    "Dartmouth College",
+    "Dartmouth Course Reviews",
+    "Layup",
+    "Layup Course Reviews",
+    "Layup Dartmouth",
+    "Layup List",
+    "Rankings",
+    "Ratings",
+    "Reviews",
+  ],
+  openGraph: {
+    description: "Based course reviews for dartmouth students",
+    images: [
+      {
+        url: "https://basedreviews.info/images/brand-body.png",
+        alt: "Based Reviews",
+      },
+    ],
+    locale: "en_US",
+    siteName: "Based Reviews",
+    title: "Based Reviews | Dartmouth's Based Course Reviews",
+    type: "website",
+    url: "https://basedreviews.info",
+  },
+  referrer: "origin",
+  robots: "index, follow",
+  title: "Based Reviews | Dartmouth's Based Course Reviews",
+  twitter: {
+    card: "summary_large_image",
+    creator: "@iamyourtoph",
+    description: "Based course reviews for dartmouth students",
+    images: [
+      {
+        alt: "Moai stones",
+        url: "https://basedreviews.info/images/brand-body.png",
+      },
+    ],
+    site: "@iamyourtoph",
+    title: "Based Reviews | Dartmouth's Based Course Reviews",
+  },
 };
 
-export default async function RootLayout({children}: Readonly<{ children: React.ReactNode; }>) {
-
+export default async function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className="h-full" suppressHydrationWarning>
-    <body className={`${inter.className} antialiased h-full bg-background`}>
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      <Header/>
-      {children}
-      <Toaster/>
-    </ThemeProvider>
-    </body>
+      <body className={`${inter.className} antialiased h-full bg-background`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header />
+          {children}
+          <Toaster />
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
